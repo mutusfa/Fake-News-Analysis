de7463eba05aa68378ec4447f57a818b2ec922d2
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2022-01-19 18:59:53 +0200

    Classical models done

---
+++ jjuoda_dl4/utils.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+BASE_DATA_DIR = Path(__file__).parent.parent / 'data'
+BASE_MODEL_DIR = Path(__file__).parent.parent / 'models'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='jjuoda-dl.4',
+    version='0.0.1',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=["jjuoda_dl4"],
+)
